7f168da6b77f00e65552945ed5e928f113bde4b6
Yuya Kitazawa <ykita993@gmail.com>
2021-12-11 22:25:32 +0900

    モジュールのインストール

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// routerとstore
+import router from "./router";
+// import store from "./store";
+
+// Element-plus install
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+const app = createApp(App);
+
+app.use(ElementPlus);
+app.use(router);
+// app.use(store);
+app.mount("#app");
